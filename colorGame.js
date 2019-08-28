437a982f4fa8a944692c612a67b228b2f42081ac
Nikolas9896 <nhlynka@gmail.com>
2019-08-28 14:22:11 +0300

    add game logic and win/lose indicator

--- colorGame.js
+++ colorGame.js
@@ -9,7 +9,27 @@ var colors = [
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
+var colorDisplay =  document.getElementById("colorDisplay");
+var clickedColor = 0;
+
+colorDisplay.textContent = pickedColor;
+
 for(var i = 0; i < squares.length; i++)
-{
+{   //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        //grab color of clicked square
+        clickedColor = this.style.backgroundColor;
+        //compare color to pickedColor
+        if(clickedColor === pickedColor)
+        {
+            alert("YOU WIN!");
+        }
+        else
+        {
+            this.style.backgroundColor = "#232323";
+        }
+    });
 }
